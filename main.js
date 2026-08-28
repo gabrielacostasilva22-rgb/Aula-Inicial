4fb316c4a5c0bf8e553f97d119f4c954d1dc683b
gabrielacostasilva22-rgb <gabriela.costa.silva22@escola.pr.gov.br>
2026-08-28 12:31:09 +0000

    ajusta lógica de reinício do jogo

--- main.js
+++ main.js
@@ -11,18 +11,18 @@ const restartButton = document.querySelector('.restart');
 const gameOver = document.querySelector('.game-over');
 // Seleciona o container da tela de Game Over
 
-// ===== DEFINIÇÃO DE ÁUDIOS =====
+//  DEFINIÇÃO DE ÁUDIOS 
 // Cria novos objetos de áudio que serão tocados em diferentes momentos do jogo
 const audioStart = new Audio('./sound/audio_theme.mp3');
 // Áudio da música tema que toca durante o jogo
 const audioGameOver = new Audio('./sound/audio_gameover.mp3');
 // Áudio que toca quando o jogador bate no cano
 
-// ===== VARIÁVEIS DE CONTROLE DO JOGO =====
+//  VARIÁVEIS DE CONTROLE DO JOGO 
 // ID do intervalo (setInterval) que executa a lógica principal do jogo
 let gameInterval = null;
 
-// ===== FUNÇÃO PARA INICIAR O JOGO =====
+// FUNÇÃO PARA INICIAR O JOGO 
 // Esta função inicia uma nova partida do jogo
 const startGame = () => {
   // Adiciona a animação do cano que faz ele se mover da direita para a esquerda
@@ -57,3 +57,4 @@ const restartGame = () => {
   mario.src = './img/mario.gif';
   mario.style.width = '150px';
   mario.style.bottom = '0';
+}
